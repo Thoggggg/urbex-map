e3956a2ed97baacb37f3c24fb911bdab9bbeee1b
Thoggggg <garnier.theo92@gmail.com>
2025-09-15 15:10:48 +0200

    Better start script

--- local-dev/start.sh
+++ local-dev/start.sh
@@ -7,14 +7,27 @@ set -e
 
 echo "--- Starting local development database ---"
 # Use the -f flag to specify which compose file to use
-# Use the -p flag to give the project a unique name to avoid conflicts
 sudo docker compose -f "$(dirname "$0")/docker-compose.db.yml" -p urbex-dev up -d
 
+# Downloading dependencies
+echo "--- Downloading dependencies ---"
+npm install
 
 # Initialize the database
+echo "--- Initializing database ---"
 cd server; npx dotenv-cli -e .env.development -- npx prisma migrate dev
 cd ..
   
+
+ending()
+{
+    echo "--- Stopping local development database ---"
+    # Use the -f flag to specify which compose file to use
+    sudo docker compose -f "$(dirname "$0")/docker-compose.db.yml" -p urbex-dev down >> /dev/null
+}
+
+trap ending INT
+
 echo ""
 echo "--- Starting frontend and backend with npm ---"
 # Run the main npm dev command from the project root
