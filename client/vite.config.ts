27de46bd7a6910fed5761d51f28914303ffa26f4
Thoggggg <garnier.theo92@gmail.com>
2025-10-04 19:13:54 +0200

    To work in local

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // [THE FIX] This proxy configuration is the key.
+    proxy: {
+      // Any request starting with /api will be proxied
+      '/api': {
+        target: 'http://localhost:3001', // The address of your backend server
+        changeOrigin: true, // Recommended for virtual hosts
+      },
+      // We also need to proxy the /uploads route for images
+      '/uploads': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+      }
+    }
+  }
 })
